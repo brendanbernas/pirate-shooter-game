984090093430b25a946c14c026e389aad35241d5
brendanbernas <brendan.bernas@georgebrown.ca>
2017-10-09 13:45:40 -0400

    Initial commit
    Complete with moveable user ship

---
+++ Assignment 1/Assets/Scripts/OceanController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OceanController : MonoBehaviour {
+
+	[SerializeField]
+	private float speed = -0.1F;
+	[SerializeField]
+	private float startX = 0;
+	[SerializeField]
+	private float endX = 100;
+
+	private Vector2 currentPosition;
+	private Transform _transform;
+	// Use this for initialization
+	void Start () {
+		_transform = this.gameObject.GetComponent<Transform>();
+		currentPosition = _transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//get current position and move it left
+		currentPosition = _transform.position;
+		currentPosition += new Vector2 (speed, 0);
+
+		//if the current position is greater than endX reset the current position to start
+		if (currentPosition.x < endX)
+			ResetCurrentPosition ();
+
+		//apply the transformation to the object
+		_transform.position = currentPosition;
+	}
+
+	void ResetCurrentPosition(){
+		currentPosition = new Vector2 (startX, 0);
+	}
+}
---
+++ Assignment 1/Assets/Scripts/UserShipController.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UserShipController : MonoBehaviour {
+	
+	private Rigidbody2D shipRb;
+
+	// Use this for initialization
+	void Start () {
+		shipRb = this.gameObject.GetComponent<Rigidbody2D> ();
+	}
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void FixedUpdate(){
+		float force = 2.0f;
+		float userInputVertical = Input.GetAxis ("Vertical");
+		float userInputHorizontal = Input.GetAxis ("Horizontal");
+
+		/*
+		Vector2 worldRightDirection = this.transform.TransformDirection (Vector2.right);
+		Vector2 worldLeftDirection = this.transform.TransformDirection (Vector2.left);
+
+
+		//adding forces for right and left
+		if (userInputVertical > 0) {
+			shipRb.AddForce (worldRightDirection * force);
+			shipRb.constraints = RigidbodyConstraints2D.None;
+		} 
+		else if (userInputVertical < 0) {
+			shipRb.AddForce (worldLeftDirection * force);
+			shipRb.constraints = RigidbodyConstraints2D.None;
+		}
+		else
+			shipRb.constraints = RigidbodyConstraints2D.FreezePositionX;
+		
+
+		//shipRb.AddForce (worldRightDirection * force);
+
+		//adding rotation
+		if (userInputHorizonal > 0)
+			shipRb.MoveRotation (shipRb.rotation - 65 * Time.fixedDeltaTime);
+		else if (userInputHorizonal < 0)
+			shipRb.MoveRotation (shipRb.rotation + 65 * Time.fixedDeltaTime);
+		*/
+
+		if (userInputVertical > 0)
+			shipRb.AddForce (Vector2.right * force);
+		else if (userInputVertical < 0)
+			shipRb.AddForce (Vector2.left * force);
+
+		if (userInputHorizontal > 0)
+			shipRb.AddForce (Vector2.down * force);
+		else if (userInputHorizontal < 0)
+			shipRb.AddForce (Vector2.up * force);
+	}
+}
